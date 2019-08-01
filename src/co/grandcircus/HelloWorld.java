deaafa7727006a3cd7250195df00bd81e93f9e09
Chris <cciric44@gmail.com>
2019-08-01 11:52:41 -0400

    with print statements

--- src/co/grandcircus/HelloWorld.java
+++ src/co/grandcircus/HelloWorld.java
@@ -3,8 +3,8 @@ package co.grandcircus;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello world");
+		System.out.println("my name is chris");
 	}
 
 }
